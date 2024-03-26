45901e1bbd00264c30fddbfc7dea0bb626f89fb7
Patricia Muresan <patriciamariamrsn@gmail.com>
2024-03-26 17:12:01 -0400

    change the font-size and colors

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,13 +3,13 @@ function NavBar() {
       <nav className="container">
         <div className="list-menu">
           <a href="https://www.w3schools.com/html/html_links.asp">About</a> 
-          <a href="https://www.w3schools.com/html/html_links.asp">Project</a>
+          <a href="https://www.w3schools.com/html/html_links.asp">Projects</a>
           <a href="https://www.w3schools.com/html/html_links.asp">Contact</a>
         </div>
         <p id="mode"><i class="fa-solid fa-sun"></i>MODE</p>
       </nav>
       
-      
+      // <i class="fa-regular fa-moon"></i> // use this for night mode
   
   );
 }
