5dd7acba28574f92e4536f04bfcdc887a8ba9fc3
Patricia Muresan <patriciamariamrsn@gmail.com>
2024-03-26 17:17:19 -0400

    test the links

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,9 +2,9 @@ function NavBar() {
   return (
       <nav className="container">
         <div className="list-menu">
-          <a href="https://www.w3schools.com/html/html_links.asp">About</a> 
-          <a href="https://www.w3schools.com/html/html_links.asp">Projects</a>
-          <a href="https://www.w3schools.com/html/html_links.asp">Contact</a>
+          <a href="https://www.google.com/">About</a> 
+          <a href="https://www.google.com/">Projects</a>
+          <a href="https://www.google.com/">Contact</a>
         </div>
         <p id="mode"><i class="fa-solid fa-sun"></i>MODE</p>
       </nav>
