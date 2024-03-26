1df6ec93ef124b3e1a19593f7d2472f4559339f7
Patricia Muresan <patriciamariamrsn@gmail.com>
2024-03-26 14:36:34 -0400

    create the navbar menu

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <div>
+      <NavBar />
       <h1>Hello frontend</h1>
     </div>
   );
---
+++ src/components/NavBar.js
@@ -0,0 +1,17 @@
+function NavBar() {
+  return (
+    <nav id="nav-menu">
+      <div>
+        <ul>
+          <li>About</li>
+          <li>Project</li>
+          <li>Contact</li>
+        </ul>
+      </div>
+      <div>
+        <p>Mode ICON LIGHT/DARK</p>
+      </div>
+    </nav>
+  );
+}
+export default NavBar;
