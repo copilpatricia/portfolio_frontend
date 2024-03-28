9d5967c72388f25ac4aea19b98d701ddb23331d5
Patricia Muresan <patriciamariamrsn@gmail.com>
2024-03-28 16:43:22 -0400

    transition title

--- src/components/About.js
+++ src/components/About.js
@@ -2,14 +2,17 @@ function About() {
   return (
     <div className="container-about">
       <div className="animated-text">
-        Hello, I am <div>Patricia Maria Muresan</div>
+        Hello, I am <span></span>    
       </div>
-      <div className="description-about">
-        I am a passionate software engineer.I love building apps that are
-        delightful to use.
-      </div>
+
     </div>
   );
 }
 
 export default About;
+
+
+//{/* <div className="description-about">
+//I am a passionate software engineer.I love building apps that are
+//delightful to use.
+//</div> */}
