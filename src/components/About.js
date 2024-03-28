46da7d7f907c7e87da8f5ec4f8e85f433f1ba3bf
Patricia Muresan <patriciamariamrsn@gmail.com>
2024-03-28 14:30:19 -0400

    working on animation css

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,7 @@ function About() {
   return (
     <div className="container-about">
       <div className="animated-text">
-        Hello, I am <span></span>
+        Hello, I am <div>Patricia Maria Muresan</div>
       </div>
       <div className="description-about">
         I am a passionate software engineer.I love building apps that are
