9d5268ba80838e580fe6d7bc6fb0c0b3a68dd257
Patricia Muresan <patriciamariamrsn@gmail.com>
2024-03-28 20:34:32 -0400

    set the colors

--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,14 @@
 function About() {
   return (
-    <div className="container-about">
-      <div className="fixed-text">Hello, I am</div>
-      <div class="animated-text">
-        <div>
-          <span>Patricia-Maria Muresan</span>
+    <div className="main-container">
+      <div className="container-about">
+        <div className="fixed-text">Hello, I am</div>
+        <div class="animated-text">
+          <div>
+            <span>Patricia-Maria Muresan</span>
+          </div>
         </div>
+        <div className="description-about">I am a passionate software engineer.I love building apps that are delightful to use.</div>
       </div>
     </div>
   );
