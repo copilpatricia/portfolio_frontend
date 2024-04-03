f64c38a2edbe214abae86a0ff4b962fde423f304
Patricia Muresan <patriciamariamrsn@gmail.com>
2024-04-03 17:17:32 -0400

    change the color of span::after when the btn is clicked using inline styling

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,4 @@
-
-
 function About(props) {
-
   return (
     <div className="main-container">
       <div className="container-about">
@@ -11,12 +8,30 @@ function About(props) {
             <span>Patricia-Maria Muresan</span>
           </div>
         </div>
-        <div className="description-about">I am a passionate software engineer.I love building apps that are delightful to use.</div>
+        <div className="description-about">
+          I am a passionate software engineer.I love building apps that are
+          delightful to use.
+        </div>
       </div>
+{/* using the inline style to change the color of the span::after effect when button is clicked  */}
+      <style>
+        {`
+        .animated-text span:after {
+          content: "";
+          position: absolute;
+          left: 0;
+          height: 100%;
+          border-left: 2px solid #faf3dd;
+          right: 0;
+          background-color: ${props.afterBackgroundColor};
+          animation: 4s textType;
+          animation-iteration-count: infinite;
+          animation-timing-function: steps(10) ;
+          animation-fill-mode: backwards;
+        }`}
+      </style>
     </div>
   );
 }
 
 export default About;
-
-
