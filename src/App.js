11ffed70954e3eeade1078c19b9a974982663c2d
Patricia Muresan <patriciamariamrsn@gmail.com>
2024-04-25 21:18:21 -0400

    change the icon based on the color

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import './App.css';
-import NavBar from './components/NavBar';
-import About from './components/About';
-import {useState} from 'react'
+import "./App.css";
+import NavBar from "./components/NavBar";
+import About from "./components/About";
+import { useState } from "react";
 
 function App() {
-  const [backgroundColor, setBackgroundColor] = useState('#e6bdb9')
-  const [afterBackgroundColor, setAfterBackgroundColor] = useState('#e6bdb9');
-
+  const [backgroundColor, setBackgroundColor] = useState("#e6bdb9");
+  const [afterBackgroundColor, setAfterBackgroundColor] = useState("#e6bdb9");
 
   const handleButtonClick = () => {
-    setBackgroundColor(backgroundColor === '#e6bdb9' ? 'black' : "#e6bdb9")
-    setAfterBackgroundColor(backgroundColor === '#e6bdb9' ? 'black' : "#e6bdb9")
-  }
-
+    setBackgroundColor(backgroundColor === "#e6bdb9" ? "black" : "#e6bdb9");
+    setAfterBackgroundColor(
+      backgroundColor === "#e6bdb9" ? "black" : "#e6bdb9"
+    );
+  };
 
   return (
-    <div id="main-div" style={{backgroundColor}}>
-      <NavBar handleButtonClick={handleButtonClick} backgroundColor={backgroundColor} />
+    <div id="main-div" style={{ backgroundColor }}>
+      <NavBar
+        handleButtonClick={handleButtonClick}
+        backgroundColor={backgroundColor}
+      />
       <About afterBackgroundColor={afterBackgroundColor} />
     </div>
   );
 }
 
 export default App;
-
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,22 +1,27 @@
 function NavBar(props) {
-  
   return (
-      <nav className="container">
-        <div className="list-menu">
-          <a href="https://www.google.com/">About</a> 
-          <a href="https://www.google.com/">Projects</a>
-          <a href="https://www.google.com/">Contact</a>
-        </div>
-        <button id="mode" onClick={props.handleButtonClick} style={{backgroundColor: props.backgroundColor === "#e6bdb9" ? "#e6bdb9" : "black"}}>
+    <nav className="container">
+      <div className="list-menu">
+        <a href="https://www.google.com/">About</a>
+        <a href="https://www.google.com/">Projects</a>
+        <a href="https://www.google.com/">Contact</a>
+      </div>
+      <button
+        id="mode"
+        onClick={props.handleButtonClick}
+        style={{
+          backgroundColor:
+            props.backgroundColor === "#e6bdb9" ? "#e6bdb9" : "black",
+        }}
+      >
+        {props.backgroundColor === "#e6bdb9" ? (
           <i className="fa-solid fa-sun"></i>
-        MODE</button>
-      </nav>
-      
-      // <i class="fa-regular fa-moon"></i> // use this for night mode
-      // add functionality to href 
-
-   
-  
+        ) : (
+          <i class="fa-regular fa-moon"></i>
+        )}
+        MODE
+      </button>
+    </nav>
   );
 }
 export default NavBar;
