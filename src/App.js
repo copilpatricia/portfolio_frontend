edc1a47ee524f5e24ba22ad7a3a002c1e287eb80
Patricia Muresan <patriciamariamrsn@gmail.com>
2024-04-25 21:08:01 -0400

    fixed the backgroundcolor

--- src/App.js
+++ src/App.js
@@ -6,16 +6,17 @@ import {useState} from 'react'
 function App() {
   const [backgroundColor, setBackgroundColor] = useState('#e6bdb9')
   const [afterBackgroundColor, setAfterBackgroundColor] = useState('#e6bdb9');
-  const [button, changeButton] = useState('#e6bdb9')
 
 
   const handleButtonClick = () => {
     setBackgroundColor(backgroundColor === '#e6bdb9' ? 'black' : "#e6bdb9")
     setAfterBackgroundColor(backgroundColor === '#e6bdb9' ? 'black' : "#e6bdb9")
   }
+
+
   return (
     <div id="main-div" style={{backgroundColor}}>
-      <NavBar handleButtonClick={handleButtonClick} button={button} />
+      <NavBar handleButtonClick={handleButtonClick} backgroundColor={backgroundColor} />
       <About afterBackgroundColor={afterBackgroundColor} />
     </div>
   );
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -7,7 +7,7 @@ function NavBar(props) {
           <a href="https://www.google.com/">Projects</a>
           <a href="https://www.google.com/">Contact</a>
         </div>
-        <button id="mode" onClick={props.handleButtonClick}>
+        <button id="mode" onClick={props.handleButtonClick} style={{backgroundColor: props.backgroundColor === "#e6bdb9" ? "#e6bdb9" : "black"}}>
           <i className="fa-solid fa-sun"></i>
         MODE</button>
       </nav>
